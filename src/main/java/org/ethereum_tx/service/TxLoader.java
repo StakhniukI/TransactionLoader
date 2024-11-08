9f7ba51f213eba630f719153245badf9c6d3c5e5
Ivan Stakhniuk <ivan.stakhniuk@gmail.com>
2024-11-08 14:09:16 +0200

    added find max saver transaction for block

--- src/main/java/org/ethereum_tx/service/TxLoader.java
+++ src/main/java/org/ethereum_tx/service/TxLoader.java
@@ -14,7 +14,11 @@ import org.web3j.protocol.http.HttpService;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.logging.Logger;
+
+import static com.mongodb.client.model.Filters.eq;
 
 @ApplicationScoped
 public class TxLoader {
@@ -32,20 +36,31 @@ public class TxLoader {
 
         Web3j web3 = Web3j.build(new HttpService(CHAIN_URL));
 
-        for (long i = transactionCountForBlock.longValue() - 1; i >= 0; i--) {
-            Optional<Transaction> transactionOptional = web3.ethGetTransactionByBlockNumberAndIndex(
-                            new DefaultBlockParameterNumber(lastBlock), BigInteger.valueOf(i)).send()
-                    .getTransaction();
+        ExecutorService executor = Executors.newCachedThreadPool();
 
-            if(transactionOptional.isPresent()) {
-                Transaction transaction = transactionOptional.get();
-                System.out.println(transaction.getTransactionIndex());
-                Gson gson = new Gson();
-                String json = gson.toJson(transaction);
-                Document doc = Document.parse(json);
-                getCollection().insertOne(doc);
+        executor.submit(() -> {
+            for (long i = transactionCountForBlock.longValue() - 1; i >= 0; i--) {
+                Optional<Transaction> transactionOptional = null;
+                try {
+                    transactionOptional = web3.ethGetTransactionByBlockNumberAndIndex(
+                                    new DefaultBlockParameterNumber(lastBlock), BigInteger.valueOf(i)).send()
+                            .getTransaction();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+                if (transactionOptional.isPresent()) {
+                    Transaction transaction = transactionOptional.get();
+                    System.out.println(transaction.getTransactionIndex());
+                    Gson gson = new Gson();
+                    String json = gson.toJson(transaction);
+                    Document doc = Document.parse(json);
+                    getTransactionCollection().insertOne(doc);
+                }
             }
-        }
+        });
+//
+
     }
 
     private BigInteger getLatestBlock() throws IOException {
@@ -79,7 +94,13 @@ public class TxLoader {
         return transactionCountForBlock;
     }
 
-    private MongoCollection getCollection(){
+    private Object getLastTransactionNumberForBlockLoaded(BigInteger blockNumber){
+        return getTransactionLoadedCollection().find(eq("blockNumber", blockNumber)).first();
+    }
+    private MongoCollection getTransactionCollection(){
         return mongoClient.getDatabase("ether_txs").getCollection("transactions");
+    }
+    private MongoCollection getTransactionLoadedCollection(){
+        return mongoClient.getDatabase("ether_txs").getCollection("transaction_loaded");
     }
 }
